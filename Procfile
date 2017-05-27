4da3e84c7d232083d0e42b07cdd4d6d263cd85fd
Julie Eubank <julie@sensehealth.com>
2017-05-27 14:49:39 -0400

    heroku docs

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ahca_api.wsgi --log-file -
--- ahca_api/settings.py
+++ ahca_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lc7zn3*gw!k@0sblubb1w0=n48)h+lb_ci$kco3gi&dl04guwp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -120,4 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
