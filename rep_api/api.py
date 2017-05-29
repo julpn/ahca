68919fa8fed392a0953d9330c56427e0b18fa6f1
Julie Eubank <julie@sensehealth.com>
2017-05-29 17:58:01 -0400

    adding full name search

--- rep_api/api.py
+++ rep_api/api.py
@@ -1,10 +1,11 @@
-from rest_framework.generics import (
-    ListAPIView
-)
-from rep_api.serializers import RepSerializer
-from rep_api.models import Rep
 from django.http import JsonResponse
 from django.http import Http404
+from django.db.models.functions import Concat
+from django.db.models import Q
+from rest_framework.generics import ListAPIView
+
+from rep_api.serializers import RepSerializer
+from rep_api.models import Rep
 
 class RepApi(ListAPIView):
     serializer_class = RepSerializer
@@ -12,8 +13,9 @@ class RepApi(ListAPIView):
     def get(self, request):
         rep_name = request.query_params.get('rep')
         try:
-            rep = Rep.objects.get(last_name__iexact=rep_name)
-        except Rep.DoesNotExist:
+            queryset = Rep.objects.annotate(search_name=Concat('first_name', 'last_name'))
+            rep = queryset.filter(Q(last_name__iexact=rep_name) | Q(search_name__iexact=rep_name))[0]
+        except IndexError:
             raise Http404
         serializer = RepSerializer(rep)
         return JsonResponse(serializer.data, safe=False)
