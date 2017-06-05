4c1b845c75a03d4c60f05e6afb9dec440193cc7e
Julie Eubank <julie@sensehealth.com>
2017-06-04 20:42:35 -0400

    well that didn't work

--- ahca_api/settings.py
+++ ahca_api/settings.py
@@ -56,11 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'killsconstituents.com',
-    'localhost:8000',
-    '127.0.0.1:8000'
-)
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'ahca_api.urls'
 
